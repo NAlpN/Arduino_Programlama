3698115d4eb0c96497efd1534ac406442e37ff34
AlpN <alpnn52@gmail.com>
2024-03-26 18:21:02 +0300

    Buton ile led yakma projesi

---
+++ buton-ile-led-kontrolu.ino
@@ -0,0 +1,19 @@
+#define Buton 8
+#define Led 9
+
+int buton_durumu = 0;
+
+void setup() {
+  pinMode(Buton, INPUT);
+  pinMode(Led, OUTPUT);
+}
+
+void loop() {
+  buton_durumu = digitalRead(Buton);
+
+  if(buton_durumu == 1)
+  digitalWrite(Led,HIGH);
+
+  else
+  digitalWrite(Led,LOW);
+}
